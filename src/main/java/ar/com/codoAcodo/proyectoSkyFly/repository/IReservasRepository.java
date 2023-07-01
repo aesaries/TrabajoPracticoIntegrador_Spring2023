97922388de83b915af9159198b9f1ad1e70e5576
Enzo <91644783+aesaries@users.noreply.github.com>
2023-07-01 00:29:10 -0300

    creaando Reservas repository

---
+++ src/main/java/ar/com/codoAcodo/proyectoSkyFly/repository/IReservasRepository.java
@@ -0,0 +1,9 @@
+package ar.com.codoAcodo.proyectoSkyFly.repository;
+
+import ar.com.codoAcodo.proyectoSkyFly.entity.Reservas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IReservasRepository extends JpaRepository<Reservas,Long> {
+
+
+}
