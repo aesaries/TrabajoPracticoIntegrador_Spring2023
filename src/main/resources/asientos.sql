0fc573237da787c6500d12710d9723aa77e1fbd4
Enzo <91644783+aesaries@users.noreply.github.com>
2023-07-01 00:21:19 -0300

    creando script de asientos.sql y agregando en el yml

---
+++ src/main/resources/asientos.sql
@@ -0,0 +1,23 @@
+INSERT INTO `asientos`
+(`asientos_id`, `vendido`, `tipo_De_Asiento`, `descripcion`, `vuelos_id`)
+VALUES
+    (1,false, 1, 'ventanilla', 1),
+    (2,false, 1, 'ventanilla', 1),
+    (3,false, 1, 'ventanilla', 1),
+    (4,false, 1, 'ventanilla', 1),
+    (5,false, 1, 'ventanilla', 1),
+    (6,false, 1, 'ventanilla', 1),
+    (7,false, 1, 'ventanilla', 1),
+    (8,false, 1, 'ventanilla', 1),
+    (9,false, 1, 'ventanilla', 1),
+    (10,false, 1, 'ventanilla', 1),
+    (11,false, 1, 'pasillo', 1),
+    (12,false, 1, 'pasillo', 1),
+    (13,false, 1, 'pasillo', 1),
+    (14,false, 1, 'pasillo', 1),
+    (15,false, 1, 'pasillo', 1),
+    (16,false, 1, 'pasillo', 1),
+    (17,false, 1, 'pasillo', 1),
+    (18,false, 1, 'pasillo', 1),
+    (19,false, 1, 'pasillo', 1),
+    (20,false, 1, 'pasillo', 1);
