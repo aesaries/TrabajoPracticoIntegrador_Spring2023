41ff05c11a6da6766347bf2f9a5daf758abfeede
Enzo <91644783+aesaries@users.noreply.github.com>
2023-07-02 11:55:53 -0300

    creando Reservas Dto

--- src/main/java/ar/com/codoAcodo/proyectoSkyFly/dto/request/ReservaDto.java
+++ src/main/java/ar/com/codoAcodo/proyectoSkyFly/dto/request/ReservaDto.java
@@ -1,2 +1,21 @@
-package ar.com.codoAcodo.proyectoSkyFly.dto.request;public class ReservaDto {
+package ar.com.codoAcodo.proyectoSkyFly.dto.request;
+
+import ar.com.codoAcodo.proyectoSkyFly.entity.Usuarios;
+import ar.com.codoAcodo.proyectoSkyFly.entity.Vuelos;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class ReservaDto {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String rol;
+    private String numeroVuelo;
+    private String ciudadOrigen;
+    private String ciudadDestino;
+    private String fechaSalida;
+    private Long numAsiento;
+
+
 }
