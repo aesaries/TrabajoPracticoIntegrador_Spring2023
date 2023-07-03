54f3dcf031d7d4d7705793e57ac2804653a43a96
Enzo <91644783+aesaries@users.noreply.github.com>
2023-07-03 01:46:08 -0300

    Agrego metodo para que busque x2 parametros

--- src/main/java/ar/com/codoAcodo/proyectoSkyFly/repository/IAsientosRepository.java
+++ src/main/java/ar/com/codoAcodo/proyectoSkyFly/repository/IAsientosRepository.java
@@ -1,7 +1,19 @@
 package ar.com.codoAcodo.proyectoSkyFly.repository;
 
 import ar.com.codoAcodo.proyectoSkyFly.entity.Asientos;
+import ar.com.codoAcodo.proyectoSkyFly.entity.Vuelos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface IAsientosRepository extends JpaRepository<Asientos,Long> {
+ List<Asientos> findByNumeroDeAsiento(int num);
+
+ Optional<Asientos> findByVuelosAndNumeroDeAsiento(Vuelos v, int na);
+
+
 }
+
