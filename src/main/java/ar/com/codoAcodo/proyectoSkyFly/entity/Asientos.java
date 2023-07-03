f2e56a709e7a336f0856cde09eee8d6884271624
Enzo <91644783+aesaries@users.noreply.github.com>
2023-07-03 16:57:41 -0300

    agregando enum a las entidades

--- src/main/java/ar/com/codoAcodo/proyectoSkyFly/entity/Asientos.java
+++ src/main/java/ar/com/codoAcodo/proyectoSkyFly/entity/Asientos.java
@@ -1,8 +1,8 @@
 package ar.com.codoAcodo.proyectoSkyFly.entity;
 
-import ar.com.codoAcodo.proyectoSkyFly.enums.DescripcionAsiento;
-import ar.com.codoAcodo.proyectoSkyFly.enums.EstadoAsiento;
-import ar.com.codoAcodo.proyectoSkyFly.enums.TipoAsiento;
+import ar.com.codoAcodo.proyectoSkyFly.enums.AsientoDescripcion;
+import ar.com.codoAcodo.proyectoSkyFly.enums.AsientoEstado;
+import ar.com.codoAcodo.proyectoSkyFly.enums.AsientoTipo;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -18,11 +18,11 @@ public class Asientos {
     private int numeroDeAsiento;
 
     @Enumerated(value = EnumType.ORDINAL)
-    private EstadoAsiento estadoAsiento;
+    private AsientoEstado estadoAsiento;
     @Enumerated(value = EnumType.ORDINAL)
-    private TipoAsiento tipoDeAsiento;
+    private AsientoTipo tipoDeAsiento;
     @Enumerated(value = EnumType.ORDINAL)
-    private DescripcionAsiento descripcion;
+    private AsientoDescripcion descripcion;
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "vuelos_id", nullable = false)
     private Vuelos vuelos;
--- src/main/java/ar/com/codoAcodo/proyectoSkyFly/entity/Usuarios.java
+++ src/main/java/ar/com/codoAcodo/proyectoSkyFly/entity/Usuarios.java
@@ -1,12 +1,14 @@
 package ar.com.codoAcodo.proyectoSkyFly.entity;
 
+import ar.com.codoAcodo.proyectoSkyFly.enums.UsuarioRol;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.util.Date;
 import java.util.Set;
 
 
-@Entity
+@Entity @Getter
 @Table(name = "usuarios")
 public class Usuarios {
     @Id
@@ -18,7 +20,8 @@ public class Usuarios {
     private String dni;
     private Date fechaNac;
     private String email;
-    private String rol;
+    @Enumerated(value = EnumType.STRING)
+    private UsuarioRol rol;
 
     @OneToMany(mappedBy = "usuarios", cascade = CascadeType.ALL, fetch = FetchType.EAGER)//el cascade se encarga de definir que operaciones realizadas sobre un objeto voy a propagar sobre un objeto relacionado, en este caso items.
     private Set<Reservas> reservas;
