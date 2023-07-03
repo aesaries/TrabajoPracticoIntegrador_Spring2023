1fbc13712de9dd54c7a61b6f97f197c01bbdb402
Enzo <91644783+aesaries@users.noreply.github.com>
2023-07-03 01:47:11 -0300

    modificando el vuelos repository

--- src/main/java/ar/com/codoAcodo/proyectoSkyFly/repository/IVuelosRepository.java
+++ src/main/java/ar/com/codoAcodo/proyectoSkyFly/repository/IVuelosRepository.java
@@ -4,6 +4,10 @@ import ar.com.codoAcodo.proyectoSkyFly.entity.Vuelos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IVuelosRepository extends JpaRepository<Vuelos,Long> {
+
+
 }
