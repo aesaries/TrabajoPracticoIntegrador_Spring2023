caa6829c5b82373a4a57d7f132a2274c7547e8a9
Enzo <91644783+aesaries@users.noreply.github.com>
2023-07-07 22:30:14 -0300

    Agrego lista de numeros de asientos al Dto de request

--- src/main/java/ar/com/codoAcodo/proyectoSkyFly/dto/request/ReservaDto.java
+++ src/main/java/ar/com/codoAcodo/proyectoSkyFly/dto/request/ReservaDto.java
@@ -3,13 +3,17 @@ package ar.com.codoAcodo.proyectoSkyFly.dto.request;
 import ar.com.codoAcodo.proyectoSkyFly.entity.Usuarios;
 import ar.com.codoAcodo.proyectoSkyFly.entity.Vuelos;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
-@Data
+import java.util.List;
+
+@Getter @Setter
 public class ReservaDto {
     private Long usuarioId;
     private Long vueloId;
-    private int numeroDeAsiento;
+    private List<Integer> listaAsientos;
 
 
 }
