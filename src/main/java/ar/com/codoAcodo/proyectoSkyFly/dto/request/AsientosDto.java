7ee4b29cde59556c5081dfce553d966313c87072
Rodrigo Urquia <rodrigourq@gmail.com>
2023-07-07 22:49:08 -0300

    "se agrego el metodo verAsietos"

---
+++ src/main/java/ar/com/codoAcodo/proyectoSkyFly/dto/request/AsientosDto.java
@@ -0,0 +1,21 @@
+package ar.com.codoAcodo.proyectoSkyFly.dto.request;
+
+import ar.com.codoAcodo.proyectoSkyFly.enums.AsientoDescripcion;
+import ar.com.codoAcodo.proyectoSkyFly.enums.AsientoEstado;
+import ar.com.codoAcodo.proyectoSkyFly.enums.AsientoTipo;
+import lombok.*;
+
+
+@Data
+public class AsientosDto {
+
+    private Long vuelosId;
+    private int numeroDeAsiento;
+    private AsientoDescripcion descripcion;
+    private AsientoTipo tipoDeAsiento;
+    private AsientoEstado estadoAsiento;
+
+
+    public void add(AsientosDto map) {
+    }
+}
